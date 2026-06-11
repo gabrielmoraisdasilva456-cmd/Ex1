cea12714a48796ff463e58471c6186d2b8a92941
GABRIEL MORAIS DA SILVA <gabriel.msilva173@senacsp.edu.br>
2026-06-11 16:46:45 -0300

    atualização do exercicio

--- EstruturasDeControle/EstruturaSwitch.cs
+++ EstruturasDeControle/EstruturaSwitch.cs
@@ -8,7 +8,7 @@ namespace CursoCSharp.EstruturasDeControle {
     internal class EstruturaSwitch {
         public static void Executar() {
 
-            Console.WriteLine("Escolha uma seleção: ");
+            /*Console.WriteLine("Escolha uma seleção: ");
             Console.WriteLine("1-Brasil");
             Console.WriteLine("2-Argentina");
             Console.WriteLine("3-França");
@@ -31,9 +31,25 @@ namespace CursoCSharp.EstruturasDeControle {
                 case 5:
                     Console.WriteLine("Gabriel Andrade");
                     break;
+            */
 
 
-            }
+
+                    Console.WriteLine("Quantos jogos deseja registrar?"); int quantidadeJogos = int.Parse(Console.ReadLine()); int totalGols = 0; for (int i = 1; i <= quantidadeJogos; i++) {
+                        Console.WriteLine("Foram feitos quantos gols?");
+                        int gols = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"No jogo {i}, foram feitos {gols} gols.");
+                        totalGols = totalGols + gols;
+                        if (i == quantidadeJogos) {
+                            Console.WriteLine($"Total de gols: {totalGols}");
+                        }
+
+
+
+
+
+
+                    }
 
         }
 
--- Fundamentos/EstruturaDeRepeticao.cs
+++ Fundamentos/EstruturaDeRepeticao.cs
@@ -1,37 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoCSharp.Fundamentos {
     internal class EstruturaDeRepeticao {
         public static void Executar() {
-            int contador = 1;
+            /* int contador =1;
 
+             while (contador <= 10) {
+                 Console.WriteLine(contador);
+                 contador++;
+
+             }
+
+             for (int cont = 1; cont <= 10; cont++) {
+                 Console.WriteLine(cont);
+
+
+
+             }
+
+             Console.WriteLine("Digite a quantidade de torcedores:");
+             int quantidade = int.Parse(Console.ReadLine());
+
+             for (int i = 1; i <= quantidade; i++) {
+                 Console.WriteLine($"Torcedor {i} entrou no estádio");*/
+
+            /*int contador = 1;
+            int tentativas = 3;
+            Console.WriteLine("Digite a senha");
+            string password = Console.ReadLine();
+
+            while (password != "7taz9Hvj" && contador != 3) {
+                Console.WriteLine($"Senha invalida. Você tem {tentativas-contador}"+$"Digite a senha novamente ");
+                password = Console.ReadLine();
+                if (password == "7taz9Hvj") {
+                    Console.WriteLine("Acesso permitido!");
+                    break;
+                } else {
+                    Console.WriteLine("Acesso negado!");
+                    contador++;
+                
+                }
+            }*/
+
+
+
+
+
+            /*int contador = 1;
             while (contador <= 10) {
-                Console.WriteLine(contador);
+                Console.WriteLine($"{contador}");
                 contador++;
 
+            
+            }*/
+        
+           /* int contador = 10;
+            while (contador >= 0) {
+                Console.WriteLine($"{contador}");
+                contador--;
+
+            }*/
+
+            int contador = 1;
+            while (contador <= 3) {
+                Console.WriteLine("Eu Gosto de programação");
+                contador++;
             }
 
-            for (int cont = 1; cont <= 10; cont++) {
-                Console.WriteLine(cont);
-
-               
-
-            }
-
-            Console.WriteLine("Digite a quantidade de torcedores:");
-            int quantidade = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= quantidade; i++) {
-                Console.WriteLine($"Torcedor {i} entrou no estádio");
-
-            }
 
 
-        } 
+
+        }
 
     }
 }
